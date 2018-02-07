541498b9db516a7b7700311f841d93a2535754a5
Hugh Powell <hugh@hughpowell.co.uk>
2018-02-07 09:02:56 +0000

    Fix typo

--- src/uk/co/hughpowell/railway_oriented_clj/v1/impl/result_handlers.clj
+++ src/uk/co/hughpowell/railway_oriented_clj/v1/impl/result_handlers.clj
@@ -20,7 +20,7 @@
 
 (defn success
   [{:keys [value] :as result}]
-  (if (some? value) value result)
+  (if (some? value) value result))
 
 (spec/fdef success
            :args (spec/cat :success ::result-object/result)
