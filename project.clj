e8ed177b886a45cafb8f010aef4c732dd1f4f9aa
Hugh Powell <hugh@hughpowell.co.uk>
2018-02-12 18:15:24 +0000

    Version 0.2.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uk.co.hughpowell/railway-oriented-clj "0.2.4"
+(defproject uk.co.hughpowell/railway-oriented-clj "0.2.5-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/HughPowell/railway-oriented-clj"
   :license {:name "Mozilla Public License v2.0"
